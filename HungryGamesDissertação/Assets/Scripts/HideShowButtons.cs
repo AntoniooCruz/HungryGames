6ba2f1652a6e599e33ded7cf74a8426623eec998
Unknown <acruz9823@gmail.com>
2021-04-04 19:31:36 +0100

    Game screens prototype

---
+++ HungryGamesDissertação/Assets/Scripts/HideShowButtons.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HideShowButtons : MonoBehaviour
+{
+    public Button StatsButton;
+    public Button UpgradeButton;
+
+    public void ToggleButtons()
+    {
+        StatsButton.gameObject.SetActive(!StatsButton.IsActive());
+        UpgradeButton.gameObject.SetActive(!UpgradeButton.IsActive());
+    }
+
+}
